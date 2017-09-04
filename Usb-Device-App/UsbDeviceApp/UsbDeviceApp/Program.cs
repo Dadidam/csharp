b22c3864d5e4d1e965ec7abd35359aa41a4271c8
Dadidam <joker.pnz@gmail.com>
2017-09-04 17:26:45 -0400

    Test case 'Usb-Device-App'

---
+++ Usb-Device-App/UsbDeviceApp/UsbDeviceApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsbDeviceApp
+{
+    class Program
+    {
+        static string CheckValue(int value)
+        {
+            string result = value.ToString();
+
+            if ((value % 3) == 0)
+                result = "usb";
+
+            if ((value % 5) == 0)
+                result = (value % 3) == 0 ? "usbdevice" : "device";
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 21; i++)
+            {
+                Console.WriteLine(CheckValue(i));
+            }
+        }
+    }
+}
